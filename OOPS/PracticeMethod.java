4e40cd35ceb04df35294e7de1fb0f18d2eed00e2
Deepak meena <deepakmeenaa78@gmail.com>
2024-07-09 23:06:27 +0530

    oops add

---
+++ OOPS/PracticeMethod.java
@@ -0,0 +1,20 @@
+
+class D {
+	public staic void main(String args []){
+		System.out.println("D Class");
+	}	
+}
+class A{
+	public static void main(String args []){
+		System.out.println("Class A");
+	}
+}
+class B{
+	public static void main(String args []){
+		SYstem.out.println("Class B ");
+	}
+}
+class C {
+	Public staic void main(String args []){
+	}
+}
